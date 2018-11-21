f938014418374290d7ffb407af935c0310bcc3bf
Joris Klein Tijssink <github@vup.io>
2018-11-21 13:34:54 +0100

    Add player structs and a single player.

--- src/game.c
+++ src/game.c
@@ -1,10 +1,15 @@
 #include "game.h"
+#include "player.h"
 #include <stdbool.h>
 
 static bool should_update = false;
+static player_t *player;
 
 // Initialize the game state.
 void game_init() {
+    // Create the player and show the lives on the 7-segment display.
+    player = player_new();
+    player_show_lives(player); // Never updated, so this is fine.
 }
 
 // Update the game, or do nothing if an update hasn't been triggered.
@@ -18,7 +23,9 @@ void game_update() {
 
     // TODO: Handle input.
     // TODO: Handle networking.
-    // TODO: Update all entities.
+
+    // Update all entities.
+    player_update(player);
 }
 
 // Trigger a game update the next time game_update() is called.
