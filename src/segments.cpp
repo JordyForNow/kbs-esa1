34cd1a05a74eeb3efd4e79f5f99988f7cc1759c6
Joris Klein Tijssink <github@vup.io>
2018-11-21 13:15:29 +0100

    Add 7-segment display functions

---
+++ src/segments.cpp
@@ -0,0 +1,27 @@
+#include "segments.h"
+#include <Wire.h>
+
+static uint8_t segments_numbers[] = {
+    0b10001000, // 0
+	0b11101011, // 1
+	0b01001100, // 2
+	0b01001001, // 3
+	0b00101011, // 4
+	0b00011001, // 5
+	0b00011000, // 6
+	0b11001011, // 7
+	0b00001000, // 8
+	0b00001001, // 9
+};
+
+void segments_show(uint8_t num) {
+    Wire.beginTransmission(56);
+    Wire.write((unsigned int) segments_numbers[num]);
+    Wire.endTransmission();
+}
+
+void segments_hide() {
+    Wire.beginTransmission(56);
+    Wire.write((unsigned int) 0xFF);
+    Wire.endTransmission();
+}
