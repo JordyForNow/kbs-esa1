615e91529aab97c9ee7dd1d9809005e2fb7b614c
matthijs prent <matthijsprent@hotmail.com>
2018-11-26 09:30:08 +0100

    more changes after the review

--- src/grid.cpp
+++ src/grid.cpp
@@ -2,83 +2,82 @@
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
-cellTypes_t grid_layout[GRID_WIDTH][GRID_HEIGHT];  // The array where the grid items are stored.
+cell_type_t grid_layout[GRID_WIDTH][GRID_HEIGHT];  // The array where the grid items are stored.
 
 // Function to initialise the twodimentional array and draw the walls.
 void grid_init() {
     for (int i = 0; i < GRID_HEIGHT; i++) {
         for (int j = 0; j < GRID_WIDTH; j++) {  // Make verticale walls.
             if (j == 0 || j == (GRID_WIDTH - 1)) {
-                change_cell(j, i, wall);
+                change_cell(j, i, WALL);
             }
             if (i == 0 || i == (GRID_HEIGHT - 1)) {  // Make horizontal walls.
-                change_cell(j, i, wall);
+                change_cell(j, i, WALL);
             }
 
             if (i > 0 && i < (GRID_HEIGHT - 1) && j > 0 && j < (GRID_WIDTH - 1)) {  // If it isn't a sidewall, make a sidewall.
                 if (i % 2 == 0 && j % 2 == 0) {
-                    change_cell(j, i, wall);
+                    change_cell(j, i, WALL);
                 } else {  // Make box or make empty
-                    grid_layout[j][i] = empty;
+                    grid_layout[j][i] = EMPTY;
                     randomSeed(TCNT1);
                     int box_bool = random(0, 2);
                     if (box_bool) {
-                        change_cell(j, i, box);
+                        change_cell(j, i, BOX);
                     } else {
-                        grid_layout[j][i] = empty;
+                        grid_layout[j][i] = EMPTY;
                     }
                 }
             }
         }
     }
     // Clear the corners so a player can start and doesn't directly get hit by a bomb.
-    for (int k = 1; k < 4; k++) {
-        int change_width = GRID_WIDTH - 1 - k;
-        int change_height = GRID_HEIGHT - 1 - k;
+    for (int i = 1; i < 4; i++) {
+        int change_width = GRID_WIDTH - 1 - i;
+        int change_height = GRID_HEIGHT - 1 - i;
 
-        change_cell(k, 1, empty);  // Clear horizontal y=1
-        change_cell(change_width, 1, empty);
+        change_cell(i, 1, EMPTY);  // Clear horizontal y=1
+        change_cell(change_width, 1, EMPTY);
 
-        change_cell(k, (GRID_HEIGHT - 2), empty);  // Clear horizontal y=(GRID_HEIGHT-2).
-        change_cell(change_width, (GRID_HEIGHT - 2), empty);
+        change_cell(i, (GRID_HEIGHT - 2), EMPTY);  // Clear horizontal y=(GRID_HEIGHT-2).
+        change_cell(change_width, (GRID_HEIGHT - 2), EMPTY);
 
-        change_cell(1, k, empty);  // Clear verticale x=1.
-        change_cell(1, change_height, empty);
+        change_cell(1, i, EMPTY);  // Clear verticale x=1.
+        change_cell(1, change_height, EMPTY);
 
-        change_cell((GRID_WIDTH - 2), k, empty);  // Clear verticale x=(GRID_WIDTH-2).
-        change_cell((GRID_WIDTH - 2), change_height, empty);
+        change_cell((GRID_WIDTH - 2), i, EMPTY);  // Clear verticale x=(GRID_WIDTH-2).
+        change_cell((GRID_WIDTH - 2), change_height, EMPTY);
     }
 }
 
 // Draw any cell, x -> horizontal coord, y-> verticale coord, type -> what needs to be drawn.
-void draw_cell(int x, int y, cellTypes_t type) {
+void draw_cell(int x, int y, cell_type_t type) {
     x = CELL_SIZE * x;
     y = CELL_SIZE * y;
 
     switch (type) {
-        case bomb:
+        case BOMB:
             draw_rect(x, y, ILI9341_LIGHTGREY);
             draw_circle(x, y, ILI9341_OLIVE);
             break;
-        case wall:
+        case WALL:
             draw_rect(x, y, ILI9341_DARKGREY);
             break;
-        case box:
+        case BOX:
             draw_rect(x, y, ILI9341_NAVY);
             break;
-        case empty:
+        case EMPTY:
             draw_rect(x, y, ILI9341_BLACK);
             break;
-        case exploding_bomb:
-            draw_exploding_bomb(x, y);
+        case EXPLODING_BOMB:
             draw_rect(x, y, ILI9341_WHITE);
             break;
     }
 }
 
 // function used to change the cell. returns 1 if succeeded, 0 if failed because cell is a wall.
-int change_cell(int x, int y, cellTypes_t change_to) {
-    if (grid_layout[x][y] != wall) {
+int change_cell(int x, int y, cell_type_t change_to) {
+    if (grid_layout[x][y] != WALL) {
         grid_layout[x][y] = change_to;
         draw_cell(x, y, change_to);
         return 1;
@@ -86,7 +85,7 @@ int change_cell(int x, int y, cellTypes_t change_to) {
     return 0;
 }
 
-cellTypes_t get_cell_content(int x, int y) {
+cell_type_t get_cell_content(int x, int y) {
     return grid_layout[x][y];
 }
 
@@ -112,27 +111,4 @@ void draw_player_two(int x, int y) {
     x += (CELL_SIZE / 2);
     y += (CELL_SIZE / 2);
     tft.fillCircle(x, y, (CELL_SIZE / 2), ILI9341_GREEN);
-}
-
-
-void draw_wall(int x, int y) {
-    tft.fillRect(x, y, CELL_SIZE, CELL_SIZE, ILI9341_DARKGREY);
-}
-
-void draw_bomb(int x, int y) {
-    tft.fillRect(x, y, CELL_SIZE, CELL_SIZE, ILI9341_LIGHTGREY);
-    x += (CELL_SIZE / 2);
-    y += (CELL_SIZE / 2);
-    tft.fillCircle(x, y, (CELL_SIZE / 2), ILI9341_OLIVE);
-}
-void draw_exploding_bomb(int x, int y) {
-    tft.fillRect(x, y, CELL_SIZE, CELL_SIZE, ILI9341_WHITE);
-}
-
-void draw_box(int x, int y) {
-    tft.fillRect(x, y, CELL_SIZE, CELL_SIZE, ILI9341_NAVY);
-}
-
-void draw_empty(int x, int y) {
-    tft.fillRect(x, y, CELL_SIZE, CELL_SIZE, ILI9341_BLACK);
 }
--- src/grid.h
+++ src/grid.h
@@ -4,36 +4,31 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 #include <Arduino.h>
-#include <stdint.h>
-#include <stdio.h>
+//#include <stdint.h>
+//#include <stdio.h>
 
 #include "defines.h"
 
 extern Adafruit_ILI9341 tft;
 
 // The possible items a cell can contain.
-typedef enum cell_types {
-    bomb,
-    wall,
-    box,
-    empty,
-    exploding_bomb,
-} cellTypes_t;
+typedef enum  {
+    BOMB,
+    WALL,
+    BOX,
+    EMPTY,
+    EXPLODING_BOMB,
+} cell_type_t;
 
-void draw_cell(int x, int y, cellTypes_t type);
+void draw_cell(int x, int y, cell_type_t type);
 void draw_player_one(int x, int y);
 void draw_player_two(int x, int y);
-void draw_wall(int x, int y);
-void draw_bomb(int x, int y);
-void draw_exploding_bomb(int x, int y);
-void draw_box(int x, int y);
-void draw_empty(int x, int y);
 void draw_rect(int x, int y, uint16_t color);
 void draw_circle(int x, int y, uint16_t color);
 
-cellTypes_t get_cell_content(int x, int y);
+cell_type_t get_cell_content(int x, int y);
 
-int change_cell(int x, int y, cellTypes_t changeTo);
+int change_cell(int x, int y, cell_type_t changeTo);
 
 void grid_init();
 
