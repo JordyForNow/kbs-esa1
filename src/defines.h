a73339790771194d5ba7dbe08ebba90e35caf4f9
Joris Klein Tijssink <github@vup.io>
2018-11-26 10:53:50 +0100

    Add logging macro comments

--- src/defines.h
+++ src/defines.h
@@ -14,11 +14,16 @@
 #define INPUT_BUTTON_Z 4
 #define INPUT_BUTTON_C 5
 
+// Logging macros that only do something when DEBUG is enabled.
+// They print messages in the following style: [src/game.cpp] updating()
+// It includes the source file where the logging call originated from.
 #if DEBUG
+// If DEBUG is enabled, enable logging.
 #define LOG_INIT() Serial.begin(115200);
 #define LOG(msg) Serial.print("[" __FILE__ "] " msg);
 #define LOGLN(msg) Serial.println("[" __FILE__ "] " msg);
 #else
+// If DEBUG is disabled, don't log anything.
 #define LOG_INIT() ;
 #define LOG(msg) ;
 #define LOGLN(msg) ;
