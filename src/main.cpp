27951d4203234829a32c60663b4e9325b19467a7
matthijs prent <matthijsprent@hotmail.com>
2018-12-05 11:00:11 +0100

    removed unused includes

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,6 @@
 #include "segments.h"
 #include "touch.h"
 
-#include <Arduino.h>
-#include <Wire.h>
-#include <nunchuck_funcs.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_ILI9341.h>
-#include <Adafruit_STMPE610.h>
-#include <Arduino.h>
-#include <SPI.h>
-#include <Wire.h>
-
 void timer1_init();
 
 int main() {
