5362d539c3e158757064f48cb569fb8f24c94d47
JordyForNow <s1114360@student.windesheim.nl>
2018-12-07 10:06:02 +0100

    fixed bug

--- src/game.h
+++ src/game.h
@@ -4,6 +4,8 @@
 #include <nunchuck_funcs.h>
 #include <stdbool.h>
 
+#include "player.h"
+
 typedef enum {
     GAME_STATE_RUNNING,
     GAME_STATE_WON,
@@ -19,6 +21,7 @@ void game_trigger_network_update();
 void opponent_move(uint8_t x, uint8_t y);
 void opponent_place_bomb(uint8_t x, uint8_t y);
 void opponent_lose_live(uint8_t x, uint8_t y);
+player_t *get_opponent();
 
 game_state_t game_get_state();
 
