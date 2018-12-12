2ac8fe12dbd7812ad964cb8056f128e9cea316f4
JordyForNow <s1114360@student.windesheim.nl>
2018-12-12 13:16:50 +0100

    no message

--- src/world.cpp
+++ src/world.cpp
@@ -59,7 +59,6 @@ void world_generate(world_t *world, unsigned long seed) {
     }
 
     world->boxes = world_count_boxes(world);
-    world->boxes = 1;
 }
 
 uint8_t world_count_boxes(world_t *world) {
